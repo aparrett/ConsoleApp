f0c0a279781cd4f87e0cf18d80838d270a585fe3
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-17 11:04:52 -0500

    Create switch statement

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string myVar = "test";
+            Console.WriteLine("This is a {0} message.", myVar);
+
+            string color = "purple";
+            switch (color)
+            {
+                case "green":
+                    Console.WriteLine($"Color is {color}");
+                    break;
+
+                case "blue":
+                    Console.WriteLine($"Color is {color}");
+                    break;
+
+                default:
+                    Console.WriteLine($"This is the default message");
+                    break;
+            }
         }
     }
 }
