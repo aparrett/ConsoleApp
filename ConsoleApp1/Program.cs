1c7dbd0c8e45910fa13b4b8d0a4c4b0f215f983c
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-19 21:22:09 -0500

    Practice with Lists and list methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,53 +7,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var numbers = new[] { 1, 2, 3, 4 };
-            var numbers2 = new[] {4, 5, 6};
-
-            // Length.
-            Console.WriteLine("Length:" + numbers.Length);
-
-            // IndexOf()
-            var index = Array.IndexOf(numbers, 5);
-            Console.WriteLine(index);
-
-            // Clear()
-            // Sets values to 0 starting at 2nd parameter, and setting however many values are given in the 3rd
-            // parameter.
-            Array.Clear(numbers, 0, 2);
-
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
             foreach (var n in numbers)
             {
                 Console.WriteLine(n);
             }
 
-            // Copy()
-            // Copies values from first array to second array, overwriting
-            // the original values.
-            Array.Copy(numbers, numbers2, 2);
-            Console.WriteLine("Copy");
-            foreach (var n in numbers2)
+            
+            Console.WriteLine("index = " + numbers.LastIndexOf(1));
+            Console.WriteLine("count = " + numbers.Count);
+
+            for (var i = 0; i < numbers.Count; i++)
             {
-                Console.WriteLine(n);
+                if (numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
             }
 
-            numbers2[0] = 10;
-
-            // Sort()
-            Console.WriteLine("Sort");
-            Array.Sort(numbers2);
-            foreach (var n in numbers2)
-            {
-                Console.WriteLine(n);
-            }
-
-            // Reverse()
-            Console.WriteLine("Reverse");
-            Array.Reverse(numbers2);
-            foreach (var n in numbers2)
-            {
-                Console.WriteLine(n);
-            }
+            var s = string.Join(" ", numbers);
+            Console.WriteLine(s);
         }
     }
 }
