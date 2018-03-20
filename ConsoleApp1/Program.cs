55f41192f21d95305429d1550705e9fdcd331707
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-19 21:03:27 -0500

    Practice with array methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,26 +6,53 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var random = new Random();
-            var randomNum = random.Next(1, 10);
-            Console.WriteLine("Please enter a number");
-            var userAnswer = Convert.ToInt32(Console.ReadLine());
+            var numbers = new[] { 1, 2, 3, 4 };
+            var numbers2 = new[] {4, 5, 6};
 
-            for (var i = 1; i < 4; i++)
+            // Length.
+            Console.WriteLine("Length:" + numbers.Length);
+
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 5);
+            Console.WriteLine(index);
+
+            // Clear()
+            // Sets values to 0 starting at 2nd parameter, and setting however many values are given in the 3rd
+            // parameter.
+            Array.Clear(numbers, 0, 2);
+
+            foreach (var n in numbers)
             {
-                if (userAnswer == randomNum)
-                {
-                    Console.WriteLine("You win!");
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Sorry. Please try again.");
-                    userAnswer = Convert.ToInt32(Console.ReadLine());
-                }
+                Console.WriteLine(n);
             }
 
-            Console.WriteLine("You lose");
+            // Copy()
+            // Copies values from first array to second array, overwriting
+            // the original values.
+            Array.Copy(numbers, numbers2, 2);
+            Console.WriteLine("Copy");
+            foreach (var n in numbers2)
+            {
+                Console.WriteLine(n);
+            }
+
+            numbers2[0] = 10;
+
+            // Sort()
+            Console.WriteLine("Sort");
+            Array.Sort(numbers2);
+            foreach (var n in numbers2)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Reverse()
+            Console.WriteLine("Reverse");
+            Array.Reverse(numbers2);
+            foreach (var n in numbers2)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
