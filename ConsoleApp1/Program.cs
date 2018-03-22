db3d832dce84c552417f7f1a287b4016321e8731
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-22 18:24:26 -0500

    Practice using StringBuilder class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using CSharpFundamentals;
 
 namespace ConsoleApp1
@@ -8,9 +9,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var sentence = "This is going to be a really really really really really really long text.";
-            var summary = StringUtility.SummarizeText(sentence, 25);
-            Console.WriteLine(summary);
+            var builder = new StringBuilder("Hello World");
+
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+
+            builder.Replace('-', '+');
+            builder.Remove(0, 10);
+            builder.Insert(0, new String('-', 10));
+            Console.WriteLine(builder);
+            Console.WriteLine(builder[0]);
         }
     }
 }
