cae535e2aa96785e7b508c6021bb97d721225cb7
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-21 22:09:19 -0500

    Add string utility class and summarize text method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using CSharpFundamentals;
 
 namespace ConsoleApp1
 {
@@ -7,32 +8,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var fullName = "Anthony Parrett ";
-            Console.WriteLine("Trim: '{0}'", fullName.Trim());
-            Console.WriteLine("To Upper: '{0}'", fullName.Trim().ToUpper());
-            Console.WriteLine("To Lower: '{0}'", fullName.Trim().ToLower());
-            Console.WriteLine("To Upper: '{0}'", fullName.Trim().ToLower());
-
-            var index = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, index);
-            var lastName = fullName.Substring(index + 1);
-            Console.WriteLine("First Name: {0}. Last Name: {1}.", firstName.Trim(), lastName.Trim());
-
-            var names = fullName.Split(' ');
-            Console.WriteLine("First Name: {0}. Last Name: {1}.", names[0], names[1]);
-
-            var newFullName = fullName.Replace("Parrett", "Parrettiasdf");
-            Console.WriteLine(newFullName);
-
-            if (String.IsNullOrWhiteSpace(" "))
-                Console.WriteLine("Invalid");
-
-            var str = "25";
-            var age = Convert.ToByte(str);
-            Console.WriteLine(age);
-
-            float price = 29.95f;
-            Console.WriteLine(price.ToString("C0"));
+            var sentence = "This is going to be a really really really really really really long text.";
+            var summary = StringUtility.SummarizeText(sentence, 25);
+            Console.WriteLine(summary);
         }
     }
 }
