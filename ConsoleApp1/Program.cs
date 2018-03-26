f9da6e2d8567d5486fa354b48d7ad33989546a2a
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-25 21:06:57 -0500

    Use an interface

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
+    public interface IPainter
+    {
+        void Paint();
+    }
+
+    public class Anthony : IPainter
+    {
+        public void Paint()
+        {
+            Console.WriteLine("Paint stuff");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
