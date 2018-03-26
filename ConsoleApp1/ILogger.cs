f524bc88d9ad9e90fe5a972ea21f7c82f55d7b0e
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-26 18:54:44 -0500

    Cleanup

--- ConsoleApp1/ILogger.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConsoleApp1
-{
-    public interface ILogger
-    {
-        void LogError(string message);
-        void LogInfo(string message);
-    }
-}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,12 +7,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var dbMigrator = new DbMigrator(new ConsoleLogger());
-            dbMigrator.Migrate();
-
-            var dbMigrator2 = new DbMigrator(new FileLogger("C:\\Log\\log.txt"));
-            dbMigrator2.Migrate();
-
 
         }
     }
