550aa069cd5b3cb71a902c990ea6bbd30013588e
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-30 13:39:11 -0500

    Use different error messages for different exceptions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,9 +9,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            DateTime? birthday = null;
-            DateTime date2 = birthday ?? DateTime.Today;
-            Console.WriteLine(date2);
+            try
+            {
+                var calculator = new Calculator();
+                var result = calculator.Divide(5, 0);
+
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Sorry, cannot divide by zero.");
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine("Sorry, an unexpected arithmetic error occured.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Sorry, an unexpected error occured.");
+            }
+        }
+    }
+
+    public class Calculator
+    {
+        public int Divide(int numerator, int denomenator)
+        {
+            return numerator / denomenator;
         }
     }
 }
