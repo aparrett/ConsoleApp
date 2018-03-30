068929c216401e0216f1099042e4376c93f7dbbe
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-30 14:03:59 -0500

    Throw custom exception

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,19 +12,47 @@ namespace ConsoleApp1
         {
             try
             {
-                using (var streamReader = new StreamReader(@"C:\file.txt"))
-                {
-                    var content = streamReader.ReadToEnd();
-                }
-            }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine("Sorry, that file could not be found.");
+                var api = new YouTubeApi();
+                var videos = api.GetVideos("Anthony");
             }
             catch (Exception e)
             {
-                Console.WriteLine("Sorry, an unexpected error occured.");
+                Console.WriteLine(e.Message);
             }
+        }
+    }
+
+    class Video
+    {
+        public string Title { get; set; }
+    }
+
+    class YouTubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                // Access Youtube web service
+                // Read the data
+                // Create a list of Video objects
+                throw new Exception("Oops a Youtube error occured.");
+            }
+            catch (Exception e)
+            {
+                throw new YouTubeException("Could not fetch Youtube videos.", e);
+            }
+
+            return new List<Video>();
+        }
+    }
+
+    class YouTubeException : Exception
+    {
+        public YouTubeException(string message, Exception innerException)
+            :base(message, innerException)
+        {
+            
         }
     }
 }
