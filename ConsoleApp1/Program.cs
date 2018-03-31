5efec5960d7401cd7ce5fb73d200401bda091b7e
Anthony Parrett <anthonyparrett7@gmail.com>
2018-03-30 21:05:53 -0500

    Cleanup

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,49 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var api = new YouTubeApi();
-                var videos = api.GetVideos("Anthony");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-    }
 
-    class Video
-    {
-        public string Title { get; set; }
-    }
-
-    class YouTubeApi
-    {
-        public List<Video> GetVideos(string user)
-        {
-            try
-            {
-                // Access Youtube web service
-                // Read the data
-                // Create a list of Video objects
-                throw new Exception("Oops a Youtube error occured.");
-            }
-            catch (Exception e)
-            {
-                throw new YouTubeException("Could not fetch Youtube videos.", e);
-            }
-
-            return new List<Video>();
-        }
-    }
-
-    class YouTubeException : Exception
-    {
-        public YouTubeException(string message, Exception innerException)
-            :base(message, innerException)
-        {
-            
         }
     }
 }
