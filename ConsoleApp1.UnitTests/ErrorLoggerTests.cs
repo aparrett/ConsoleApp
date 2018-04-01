145b71e14d4c7da07f8a9fd2a3830a8430c89a83
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-01 18:23:31 -0500

    Add Test for Log method

---
+++ ConsoleApp1.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1.Fundamentals;
+using NUnit.Framework;
+
+namespace ConsoleApp1.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
