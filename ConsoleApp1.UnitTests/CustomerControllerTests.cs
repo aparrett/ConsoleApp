648dce17b45838265dbca4ebef35d4c9bab667a9
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-01 18:12:52 -0500

    Add Test for CustomerController.GetCustomer

---
+++ ConsoleApp1.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1.Fundamentals;
+using NUnit.Framework;
+
+namespace ConsoleApp1.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+    }
+}
