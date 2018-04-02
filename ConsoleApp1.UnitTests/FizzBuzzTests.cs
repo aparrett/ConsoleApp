e4cac2cc06f67d2a46e8cc9f8dc7484bc405dc50
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-01 19:59:10 -0500

    Add tests for FizzBuzz

---
+++ ConsoleApp1.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1.Fundamentals;
+using NUnit.Framework;
+
+namespace ConsoleApp1.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(0)]
+        [TestCase(null)]
+        public void GetOutput_NumberIsNull_ThrowArgumentNullException(int number)
+        {
+            Assert.That(() => FizzBuzz.GetOutput(number), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void GetOutput_NumberIsDivisibleBy3and5_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberIsDivisibleBy3_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberIsDivisibleBy5_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberIsNotDivisibleBy3or5_ReturnNumber()
+        {
+            var result = FizzBuzz.GetOutput(8);
+            Assert.That(result, Is.EqualTo("8"));
+        }
+    }
+}
---
+++ ConsoleApp1/Fundamentals/FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1.Fundamentals
+{
+    public class FizzBuzz
+    {
+        public static string GetOutput(int number)
+        {
+            if (number == 0 || number == null)
+                throw new ArgumentNullException();
+            if ((number % 3 == 0) && (number % 5 == 0))
+                return "FizzBuzz";
+
+            if (number % 3 == 0)
+                return "Fizz";
+
+            if (number % 5 == 0)
+                return "Buzz";
+
+            return number.ToString(); 
+        }
+    }
+}
