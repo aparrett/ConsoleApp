667cbc281b71e5c5aa87401e2cb358cdcb8c90ca
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-02 19:18:33 -0500

    Create test for OrderService PlaceOrder() using Moq

---
+++ ConsoleApp1.UnitTests/OrderServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1.Mocking;
+using Moq;
+using NUnit.Framework;
+
+namespace ConsoleApp1.UnitTests
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var storage = new Mock<IStorage>();      
+            var service = new OrderService(storage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
